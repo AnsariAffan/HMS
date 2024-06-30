6d4534d5b0b7b51a493b2c8fdf98c44ef6ed959b
AnsariAffan <mohammadaffan777@gmail.com>
2024-06-30 18:26:05 +0530

    clientdeploymentv5

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
-import "./App.css";
-
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-
 import About from "./components/About";
-import Usercalender from "./components/Usercalender";
 import Usertable from "./components/Usertable";
 import UserForm from "./components/UserForm";
 import UserDashboard from "./components/UserDashboard";
+
 import "./index.css";
+import "./App.css";
 import "./components/calendar.css";
 import "./components/Dashboard.css";
 import "./components/Usertable.css";
-import "./components/UserDashboard.css"
-import "./components/Usertable.css"
-import "./App.css"
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
-
-
+import "./components/UserDashboard.css";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-          <Dashboard><UserDashboard/></Dashboard>
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          
-          <Route path="/Usertable">
-            <Dashboard><Usertable/></Dashboard>
-          </Route>
-          <Route path="/UserForm">
-            <Dashboard><UserForm/></Dashboard>
-          </Route>
-          <Route path={`/pateint/:id`}>
-            <Dashboard><UserForm/></Dashboard>
-          </Route>
-
-        </Switch>
-        </BrowserRouter>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard>
+            <UserDashboard />
+          </Dashboard>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/Usertable">
+          <Dashboard>
+            <Usertable />
+          </Dashboard>
+        </Route>
+        <Route path="/UserForm">
+          <Dashboard>
+            <UserForm />
+          </Dashboard>
+        </Route>
+        <Route path="/pateint/:id">
+          <Dashboard>
+            <UserForm />
+          </Dashboard>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
