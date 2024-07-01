8c0556d136cc6c9f56dec40bd32b112304491b5f
AnsariAffan <mohammadaffan777@gmail.com>
2024-07-01 20:44:10 +0530

    SV1.0.2

---
+++ controllers/login.js
@@ -0,0 +1,35 @@
+const User = require("../models/userloginModel");
+
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log("Received login request:", username);
+        console.log(username, password);
+
+        // Ensure username and password are provided
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password are required" });
+        }
+
+        // Check if the user exists
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid username or password" });
+        }
+
+        // Check if the password is correct
+        const isMatch = await User.comparePassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid username or password" });
+        }
+
+        // If credentials are correct, send a success response
+        res.json({ message: "Login successful" });
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,12 @@ const { createNewEntry } = require('../controllers/createDataEntry')
 const { getAllPateints } = require('../controllers/getAllPateints')
 const { updatePateint } = require('../controllers/updatePateint')
 const { updateNewEntry } = require('../controllers/updateNewEntry')
+const { login } = require('../controllers/login')
 const router = express.Router()
 
 router.post("/createNewEntry",createNewEntry)
 router.put("/updateNewEntry",updateNewEntry)
 router.get("/getAllPateints",getAllPateints)
 router.put("/updatePateint/:id",updatePateint)
+router.post("/login",login)
 module.exports = router
