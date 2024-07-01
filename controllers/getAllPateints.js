7591f8206fd45df7e16f2db36f36283cc9fd8b02
AnsariAffan <mohammadaffan777@gmail.com>
2024-07-01 12:52:57 +0530

    SV1.0.0

--- controllers/getAllPateints.js
+++ controllers/getAllPateints.js
@@ -3,7 +3,6 @@ const newEntry = require("../models/NewEntry");
 
 exports.getAllPateints = async (req, res) => {
   try {
-    
 
     const allPateints = await newEntry.find();
 console.log(allPateints)
@@ -13,7 +12,6 @@ return res.send(allPateints)
   } catch (error) {
 
     res.status(404).json({ message: "Something went wrong" });
-
     console.error("Error fetching patients:", error); // Log the actual error
     res.status(500).json({ message: "Something went wrong", error: error.message });
 
