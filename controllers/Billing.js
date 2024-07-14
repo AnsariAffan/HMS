c467d51608f6e2d82fa6ac482575abd5e4036940
AnsariAffan <mohammadaffan777@gmail.com>
2024-07-14 19:48:08 +0530

    SV1.0.10 New bill model added

--- controllers/Billing.js
+++ controllers/Billing.js
@@ -22,7 +22,7 @@ exports.createBillingEntry = async (req, res) => {
         }
 
         // Check if there's already an existing entry with the same Bill_ID
-        const existingEntry = await billingModel.findOne({ Bill_ID: headerData.Bill_ID });
+        const existingEntry = await billingModel.findOne();
         if (existingEntry) {
             return res.json({ message: "Bill is already added" });
         } else {
