52ad1a1603b95f9fbc8373d4b0b3769e1a6a2312
AnsariAffan <mohammadaffan777@gmail.com>
2024-07-17 11:48:12 +0530

    BV1.0.10 New fields added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from "./api/store";
-
+ 
 
 ReactDOM.render(
   <React.StrictMode>
