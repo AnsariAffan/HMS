081fd1c669bcb3d7977e7dba016953f58f3eea1c
AnsariAffan <mohammadaffan777@gmail.com>
2024-07-28 21:50:15 +0530

    SV1.0.11 ENV setup

--- models/userloginModel.js
+++ models/userloginModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
+
+    
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     loginType: { type: String, required: true},
